e4cf7a74f5a1eabdd464dd7fe1c583e5b3e1a15a
Moksit Abdelhakim <moksit.abdelhakim@gmail.com>
2025-07-25 20:39:40 +0100

    Fix import tailwindcss for packages

--- apps/react-portfolio/src/app/app.tsx
+++ apps/react-portfolio/src/app/app.tsx
@@ -1,8 +1,13 @@
-
+import {Button} from '@moksit-org/ui';
+import { Eye } from 'lucide-react';
 export function App() {
   return (
     <div>
       <h1 className="text-4xl font-bold text-blue-500 mt-8">Welcom to my portfolio</h1>
+      <Button size="sm" variant="outline">
+                    <Eye className="mr-2 h-3 w-3" />
+                    Preview
+                  </Button>
     </div>
   );
 }
